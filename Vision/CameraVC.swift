52f5cbb2a095e50b68a0d6e8a50662f647f1c38b
Krystyna Kruchkovska <kruchok883@gmail.com>
2019-08-13 19:12:18 +0200

    Add Camera session

--- Vision/CameraVC.swift
+++ Vision/CameraVC.swift
@@ -11,6 +11,10 @@ import AVFoundation
 
 class CameraVC: UIViewController {
     
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
     @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var identificationLbl: UILabel!
     @IBOutlet weak var confidanceLbl: UILabel!
@@ -21,7 +25,40 @@ class CameraVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .hd1920x1080
+        
+        let backCamera = AVCaptureDevice.default(for: .video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+            }
+            
+            cameraOutput = AVCapturePhotoOutput()
+            
+            if captureSession.canAddOutput(cameraOutput) {
+                captureSession.canAddOutput(cameraOutput)
+            }
+            
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer.videoGravity = .resizeAspect
+            previewLayer.connection?.videoOrientation = .portrait
+            
+            cameraView.layer.addSublayer(previewLayer)
+            captureSession.startRunning()
+        } catch {
+            debugPrint(error)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer.frame = cameraView.bounds
+    }
 
 }
 
